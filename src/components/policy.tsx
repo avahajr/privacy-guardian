94d84c9ecba737378dc4a15c4149ab4479689beb
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2025-02-26 18:08:54 -0500

    cache policy html

--- src/components/policy.tsx
+++ src/components/policy.tsx
@@ -5,28 +5,33 @@ import { apiRequest } from "@/helpers/requests";
 import { useStorage } from "@/hooks/useStorage.ts";
 
 export default function Policy() {
-  const [policyHTML, setPolicyHTML] = useState([]);
-  const { goals, policy } = useStorage();
+  const { goals, policy, policy_html } = useStorage();
+  const [policyHTML, setPolicyHTML] = useState(policy_html);
 
   useEffect(() => {
-    const goalAndPolicy =
-      goals && policy ? { goals: goals, policy: policy } : {};
+    if (!policyHTML) {
+      const goalAndPolicy =
+        goals && policy ? { goals: goals, policy: policy } : {};
 
-    apiRequest({ endpoint: "html/policy", method: "POST", ...goalAndPolicy })
-      .then((response) => response.json())
-      .then((data) => {
-        setPolicyHTML(data.policy_html);
-      });
+      apiRequest({ endpoint: "html/policy", method: "POST", ...goalAndPolicy })
+        .then((response) => response.json())
+        .then((data) => {
+          setPolicyHTML(data.policy_html);
+          sessionStorage.setItem("policy_html", data.policy_html);
+        });
+    }
   }, []);
 
   return (
     <Card className="p-2 sticky top-48">
-      <CardHeader className={"font-medium font-mono text-xs text-default-500"}>{policy?.toLowerCase()}_policy.md</CardHeader>
-      <CardBody id={"policy-text"}>
-        <div
-          dangerouslySetInnerHTML={{ __html: policyHTML }}
-          className="text-default-700 h-[55vh] overflow-y-scroll"
-        />
+      <CardHeader className={"font-medium font-mono text-xs text-default-500"}>
+        {policy?.toLowerCase()}_policy.md
+      </CardHeader>
+      <CardBody
+        className={"text-default-700 h-[55vh] overflow-y-scroll"}
+        id={"policy-text"}
+      >
+        {policyHTML && <div dangerouslySetInnerHTML={{ __html: policyHTML }} />}
       </CardBody>
     </Card>
   );
--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,5 +1,13 @@
 import { Goal } from "@/types";
 
-export function useStorage(): { goals: Goal[] | null; policy: string | null } {
-  return { goals: JSON.parse(sessionStorage.getItem("goals") || "null"), policy: sessionStorage.getItem("policy") };
+export function useStorage(): {
+  goals: Goal[] | null;
+  policy: string | null;
+  policy_html: string | null;
+} {
+  return {
+    goals: JSON.parse(sessionStorage.getItem("goals") || "null"),
+    policy: sessionStorage.getItem("policy"),
+    policy_html: sessionStorage.getItem("policy_html"),
+  };
 }
